e2dc6086a53715607e90abfa1ae2cd76159729df
Olha Pavliuk <olia.pavliuk@gmail.com>
2020-12-26 00:56:41 +0200

    log

--- r2-shared-swift/Publication/Publication.swift
+++ r2-shared-swift/Publication/Publication.swift
@@ -144,7 +144,7 @@ public class Publication: Loggable {
         let link = self.link(withHREF: href)?
             // Uses the original href to keep the query parameters
             .copy(href: href, templated: false)
-        
+        print("TADAM Publication get: \(href)")
         return get(link ?? Link(href: href))
     }
 
